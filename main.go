b67216baa7ee7cef9b2f250fe4275ca3b4a3d823
ugniusin <ugnius.sinonis@gmail.com>
2023-04-10 14:59:51 +0300

    GraphQL Subscriptions server

--- main.go
+++ main.go
@@ -2,220 +2,145 @@ package main
 
 import (
 	"context"
-  "fmt"
+	"fmt"
 	"log"
 	"net/http"
-	"sync"
+	"runtime"
 
 	"github.com/graphql-go/graphql"
+	"github.com/redis/go-redis/v9"
 	"golang.org/x/net/websocket"
 )
 
 var schema graphql.Schema
 
+var rdb = PubSubClient()
+
 func init() {
+	subscriptionMessageType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "SubscriptionMessage",
+		Fields: graphql.Fields{
+			"channel": &graphql.Field{
+				Type: graphql.String,
+			},
+			"message": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+
 	// Define a GraphQL schema
-	fields := graphql.Fields{
-		"counter": &graphql.Field{
-			Type: graphql.String,
+	subscriptionFields := graphql.Fields{
+		"event": &graphql.Field{
+			Type: subscriptionMessageType,
 			Args: graphql.FieldConfigArgument{
-				"count": &graphql.ArgumentConfig{
-					Type: graphql.Int,
+				"channel": &graphql.ArgumentConfig{
+					Type: graphql.String,
 				},
 			},
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				// Extract the count argument from the resolver params
-				count, ok := p.Args["count"].(int)
-				if !ok {
-					count = 1
-				}
-
-				// Generate a greeting message with the specified count
-				greeting := ""
-				for i := 0; i < count; i++ {
-					greeting += "Hello, world!\n"
-				}
-
-				return greeting, nil
+				return struct {
+					Channel string
+					Message interface{}
+				}{
+					Channel: p.Args["channel"].(string),
+					Message: p.Source,
+				}, nil
 			},
 			Subscribe: func(p graphql.ResolveParams) (interface{}, error) {
-				// Extract the count argument from the resolver params
-				count, ok := p.Args["count"].(int)
-				if !ok {
-					count = 1
-				}
+				pubsub := PubSubChannel(p.Args["channel"].(string))
 
-				// Return a channel that generates the greeting message with the specified count
 				c := make(chan interface{})
 				go func() {
-					for i := 0; i < count; i++ {
-						c <- fmt.Sprintf("Hello, world! [%d]", i+1)
+					for {
+						select {
+						case <-p.Context.Done():
+							pubsub.Unsubscribe(p.Context, p.Args["channel"].(string))
+							pubsub.Close()
+
+							return
+						case message := <-pubsub.Channel():
+							c <- message.Payload
+						}
 					}
-					close(c)
 				}()
 				return c, nil
 			},
 		},
 	}
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	rootMutation := graphql.ObjectConfig{Name: "RootMutation", Fields: fields}
-	rootSubscription := graphql.ObjectConfig{Name: "RootSubscription", Fields: fields}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: graphql.Fields{}}
+	rootMutation := graphql.ObjectConfig{Name: "RootMutation", Fields: graphql.Fields{}}
+	rootSubscription := graphql.ObjectConfig{Name: "RootSubscription", Fields: subscriptionFields}
 
 	schemaConfig := graphql.SchemaConfig{
-		Query: graphql.NewObject(rootQuery),
-		Mutation: graphql.NewObject(rootMutation),
+		Query:        graphql.NewObject(rootQuery),
+		Mutation:     graphql.NewObject(rootMutation),
 		Subscription: graphql.NewObject(rootSubscription),
 	}
 	schema, _ = graphql.NewSchema(schemaConfig)
 }
 
 func main() {
-	// Initialize a mutex to synchronize access to the subscription manager
-	// var mu sync.Mutex
-
-	// Initialize a map to store the active subscriptions
-	// subscriptions := make(map[*websocket.Conn]bool)
-
 	http.Handle("/graphql", websocket.Handler(subscriptionsHandler))
 	log.Println("WebSocket server started at :8080/graphql")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func graphqlHandler(ws *websocket.Conn) {
-	defer ws.Close()
-
-	var message string
-	err := websocket.Message.Receive(ws, &message)
-	if err != nil {
-		log.Println("Failed to receive message:", err)
-		return
-	}
-
-	// Execute the GraphQL query
-	params := graphql.Params{Schema: schema, RequestString: message}
-	result := graphql.Do(params)
-
-	// Send the result back to the client
-	err = websocket.JSON.Send(ws, result)
-	if err != nil {
-		log.Println("Failed to send result:", err)
-	}
-}
-
-
 func subscriptionsHandler(ws *websocket.Conn) {
-	// Add the WebSocket connection to the subscription manager
-	//mu.Lock()
-	//subscriptions[ws] = true
-	//mu.Unlock()
-
-	// Set up a GraphQL subscription manager
-	//subMgr := graphql.NewSubscriptionManager(&schema)
-
-	// Close the WebSocket connection and remove it from the subscription manager when the client disconnects
-	//defer func() {
-	//	ws.Close()
-	//	mu.Lock()
-	//	delete(subscriptions, ws)
-	//	mu.Unlock()
-	//}()
-
-	ctx := context.WithValue(context.Background(), "counter", 0)
-
 	defer ws.Close()
-	
-	// GraphQL WebSocket message format
-	type graphqlWSMessage struct {
-		Type    string                 `json:"type"`
-		ID      string                 `json:"id,omitempty"`
-		Payload map[string]interface{} `json:"payload"`
-	}
+
+	defer func() {
+		buf := make([]byte, 1<<20)
+		runtime.Stack(buf, true)
+		fmt.Println("Stack trace of all goroutines:\n", string(buf))
+	}()
 
 	// Loop continuously and listen for incoming messages from the client
 	for {
-		log.Println("LOOP")
-
 		var message string
 		err := websocket.Message.Receive(ws, &message)
+
 		if err != nil {
 			if err.Error() == "EOF" {
 				// Ignore the error if the client disconnected
 				return
 			}
+
 			log.Println("Failed to receive message:", err)
 			return
 		}
 
-		// Parse the incoming message as a GraphQL query
-		// params := graphql.Params{Schema: schema, RequestString: message}
+		ctx, cancel := context.WithCancel(context.Background())
 
+		// Make sure to cancel the context when the Subscribe method returns
+		defer cancel()
+
+		// Parse the incoming message as a GraphQL query
 		params := graphql.Params{Schema: schema, RequestString: message, Context: ctx}
 
-		// Parse the incoming message as a GraphQL subscription query
-		//doc, err := parser.Parse(parser.ParseParams{Source: source.NewSource(&source.Source{Body: message})})
-		//if err != nil {
-		//	log.Println("Failed to parse subscription:", err)
-		//	continue
-		//}
+		subscription := graphql.Subscribe(params)
 
-		// Validate the subscription query against the schema
-		//validationResult := graphql.ValidateDocument(&schema, doc, nil)
-		//if !validationResult.IsValid {
-		//	errMsgs := gqlerrors.FormatErrors(validationResult.Errors)
-		//	log.Println("Failed to validate subscription:", errMsgs)
-		//	continue
-		//}
-		
-		// If the operation is a subscription, execute it in a separate goroutine and send the results back to the client
-		if params.OperationName == "subscription" {
-			go func() {
-				// result := graphql.Execute(params)
-				//result := graphql.Do(params)
-
-				subscriptionChannel := graphql.Subscribe(params)
-
-
-				// defer subscription.Unsubscribe()
-
-				// Create a wait group to ensure that the subscription channel is closed before exiting the function
-				wg := sync.WaitGroup{}
-				wg.Add(1)
-				defer wg.Wait()
-
-				// Loop over the subscription and write results to the WebSocket
-				for result := range subscriptionChannel {
-					err = websocket.JSON.Send(ws, result)
-					if err != nil {
-						log.Printf("failed to send subscription result, error: %v", err)
-						break
-					}
+		go func() {
+			// Loop over the subscription and write results to the WebSocket
+			for result := range subscription {
+				err = websocket.JSON.Send(ws, result)
+				if err != nil {
+					log.Printf("failed to send subscription result, error: %v", err)
+					break
 				}
-
-				// Mark the wait group as done
-				wg.Done()
-			}()
-		} else {
-			// If the operation is not a subscription, execute it synchronously and send the results back to the client
-		
-			// var message string
-			//err := websocket.Message.Receive(ws, &message)
-			//if err != nil {
-
-			//	log.Println("ELSE ERROR:", err)
-
-			//	log.Println("Failed to receive message:", err)
-			//	return
-			//}
-
-			// Execute the GraphQL query
-			//params := graphql.Params{Schema: schema, RequestString: message}
-			result := graphql.Do(params)
-
-			// Send the result back to the client
-			err = websocket.JSON.Send(ws, result)
-			if err != nil {
-				log.Println("Failed to send result:", err)
 			}
-		}
+		}()
 	}
+}
+
+func PubSubClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "password",
+		DB:       0, // use default DB
+	})
+}
+
+func PubSubChannel(channelName string) *redis.PubSub {
+	return rdb.Subscribe(context.Background(), channelName)
 }
